dee21588075299e3ad9b59bea98ed4daf5930645
vinit gaikwad <vinitgaikwad5757@gmail.com>
2025-10-25 14:46:07 +0530

    all done

--- server.js
+++ server.js
@@ -17,7 +17,12 @@ app.use((req, res, next) => {
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:3001"],
+        origin: [
+            "http://localhost:3000",
+            "http://127.0.0.1:3000",
+            "http://localhost:3001",
+            "http://192.168.0.100:3000"
+        ],
         methods: ["GET", "POST"],
         credentials: true,
         allowedHeaders: ["my-custom-header"],
